4de664e1369cbe60ab3159ad852c55ad1abe707d
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-08-01 10:49:00 +0300

    add `init.c`

---
+++ init.c
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static void warn(const char *msg) { write(2, msg, strlen(msg)); }
+
+int main(void) {
+  mount("proc", "/proc", "proc", 0, NULL);
+  mount("sysfs", "/sys", "sysfs", 0, NULL);
+  mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
+  mknod("/dev/console", S_IFCHR | 0600, makedev(5, 1));
+
+  int fd = open("/dev/console", O_RDWR);
+  if (fd < 0) {
+    warn("init: no console\n");
+  } else {
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
+    if (fd > 2)
+      close(fd);
+  }
+
+  setsid();
+  ioctl(0, TIOCSCTTY, 1);
+
+  execve("/bin/sh", (char *[]){"/bin/sh", "-i", NULL}, NULL);
+  warn("init: execve failed\n");
+
+  for (;;) {
+    if (wait(NULL) < 0 && errno == ECHILD)
+      pause();
+  }
+}
